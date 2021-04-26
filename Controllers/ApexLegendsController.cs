1a0677592ecda4fb1f999544db8fab9d7eb1e49c
Jor Sanders <it@jor.dev>
2021-04-27 00:19:42 +0200

    fix names

--- Controllers/ApexLegendsController.cs
+++ Controllers/ApexLegendsController.cs
@@ -1,45 +1,80 @@
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
-using GameDataApi.Migrations;
-using Jorkol.GameDataApi.ApexLegends.Db;
 using Jorkol.GameDataApi.ApexLegends.Models;
 using Jorkol.GameDataApi.ApexLegends.Services;
 using Jorkol.GameDataApi.Dtos;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Jorkol.GameDataApi.Controllers
 {
-    [Route("")]
+    [Route("api/[controller]")]
     [ApiController]
-    public class MainController : ControllerBase
+    public class ApexLegendsController : ControllerBase
     {
-        private readonly ILogger<MainController> logger;
-        private readonly ApexDbContext dbContext;
-        public MainController(ILogger<MainController> logger, ApexDbContext dbContext)
+        private readonly ILogger<ApexLegendsController> logger;
+        private readonly IApexMatchService matchService;
+        private readonly IApexAccountService accountService;
+
+        public ApexLegendsController(ILogger<ApexLegendsController> logger, IApexMatchService apexMatchService, IApexAccountService apexAccountService)
         {
             this.logger = logger;
-            this.dbContext = dbContext;
+            this.matchService = apexMatchService;
+            this.accountService = apexAccountService;
         }
 
         [HttpGet]
         [Route("[action]")]
-        public bool Health()
+        public async Task<ListResponse<ApexMatch>> MatchesByAccount([FromQuery(Name = "platform")] string platform, [FromQuery(Name = "name")] string name)
         {
-            return true;
+            var account = accountService.ByNameAndPlatform(name, platform);
+            var matches = await this.matchService.FromTrnAndDbAsync(account);
+            return new ListResponse<ApexMatch>(matches);
         }
 
         [HttpGet]
         [Route("[action]")]
-        public void Migrate()
+        public ListResponse<ApexMatch> MatchesFromDbByAccount([FromQuery(Name = "platform")] string platform, [FromQuery(Name = "name")] string name)
         {
-            var db = dbContext.Database;
-            db.Migrate();
+            var account = accountService.ByNameAndPlatform(name, platform);
+            var matches = this.matchService.FindByAccount(account);
+            return new ListResponse<ApexMatch>(matches);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public ListResponse<ApexAccount> Accounts()
+        {
+            var accounts = accountService.WithMatches();
+            return new ListResponse<ApexAccount>(accounts);
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<ListResponse<ApexMatch>> TrnMatchesOfAllAccounts()
+        {
+            var accounts = accountService.All();
+            var matches = await matchService.FromTrnAsync(accounts);
+
+            return new ListResponse<ApexMatch>(matches);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public ListResponse<ApexMatch> AllMatchesFromDb()
+        {
+            var matches = matchService.All();
+            return new ListResponse<ApexMatch>(matches);
+        }
+
+        [HttpDelete]
+        [Route("[action]")]
+        public void CleanDb()
+        {
+            matchService.Delete(matchService.All());
+            accountService.Delete(accountService.All());
         }
     }
 }
--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,80 +1,45 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
+using GameDataApi.Migrations;
+using Jorkol.GameDataApi.ApexLegends.Db;
 using Jorkol.GameDataApi.ApexLegends.Models;
 using Jorkol.GameDataApi.ApexLegends.Services;
 using Jorkol.GameDataApi.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Jorkol.GameDataApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("")]
     [ApiController]
-    public class ApexLegendsController : ControllerBase
+    public class MainController : ControllerBase
     {
-        private readonly ILogger<ApexLegendsController> logger;
-        private readonly IApexMatchService matchService;
-        private readonly IApexAccountService accountService;
-
-        public ApexLegendsController(ILogger<ApexLegendsController> logger, IApexMatchService apexMatchService, IApexAccountService apexAccountService)
+        private readonly ILogger<MainController> logger;
+        private readonly ApexDbContext dbContext;
+        public MainController(ILogger<MainController> logger, ApexDbContext dbContext)
         {
             this.logger = logger;
-            this.matchService = apexMatchService;
-            this.accountService = apexAccountService;
+            this.dbContext = dbContext;
         }
 
         [HttpGet]
         [Route("[action]")]
-        public async Task<ListResponse<ApexMatch>> MatchesByAccount([FromQuery(Name = "platform")] string platform, [FromQuery(Name = "name")] string name)
+        public bool Health()
         {
-            var account = accountService.ByNameAndPlatform(name, platform);
-            var matches = await this.matchService.FromTrnAndDbAsync(account);
-            return new ListResponse<ApexMatch>(matches);
+            return true;
         }
 
         [HttpGet]
         [Route("[action]")]
-        public ListResponse<ApexMatch> MatchesFromDbByAccount([FromQuery(Name = "platform")] string platform, [FromQuery(Name = "name")] string name)
+        public void Migrate()
         {
-            var account = accountService.ByNameAndPlatform(name, platform);
-            var matches = this.matchService.FindByAccount(account);
-            return new ListResponse<ApexMatch>(matches);
-        }
-
-        [HttpGet]
-        [Route("[action]")]
-        public ListResponse<ApexAccount> Accounts()
-        {
-            var accounts = accountService.WithMatches();
-            return new ListResponse<ApexAccount>(accounts);
-        }
-
-        [HttpPost]
-        [Route("[action]")]
-        public async Task<ListResponse<ApexMatch>> TrnMatchesOfAllAccounts()
-        {
-            var accounts = accountService.All();
-            var matches = await matchService.FromTrnAsync(accounts);
-
-            return new ListResponse<ApexMatch>(matches);
-        }
-
-        [HttpGet]
-        [Route("[action]")]
-        public ListResponse<ApexMatch> AllMatchesFromDb()
-        {
-            var matches = matchService.All();
-            return new ListResponse<ApexMatch>(matches);
-        }
-
-        [HttpDelete]
-        [Route("[action]")]
-        public void CleanDb()
-        {
-            matchService.Delete(matchService.All());
-            accountService.Delete(accountService.All());
+            var db = dbContext.Database;
+            db.Migrate();
         }
     }
 }
